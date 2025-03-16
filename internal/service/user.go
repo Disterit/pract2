b17505e74ec593062745dc4ec167254d2a91f450
Dister <levchikov.zhenya@mail.ru>
2025-03-16 13:42:22 +0700

    create comment

--- internal/service/user.go
+++ internal/service/user.go
@@ -29,20 +29,24 @@ func NewUserService(repo repo.User, logger *zap.SugaredLogger, cfg config.Servic
 	}
 }
 
+// регистрация пользователя
 func (s *UserService) SingUp(ctx *fiber.Ctx) error {
 	var input models.User
 
+	// парсим данные пользователя
 	if err := ctx.BodyParser(&input); err != nil {
 		s.logger.Errorw("Error parsing body", "error", err)
 		return dto.BadResponseError(ctx, dto.FieldBadFormat, err.Error())
 	}
 
+	// хэшируем пароль для БД
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
 	if err != nil {
 		s.logger.Errorw("Error hashing password", "error", err)
 		return dto.InternalServerError(ctx)
 	}
 
+	// передаем данные имя и новый хешированный пароль
 	err = s.repo.SingUp(ctx.Context(), input.Username, string(hashPassword))
 	if err != nil {
 		s.logger.Errorw("Error creating user", "error", err)
@@ -56,32 +60,37 @@ func (s *UserService) SingUp(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(response)
 }
 
+// структура написана для задания, потому что в одном из заданий был пункт что нужно найти все задания пользователя по username
 type CustomClaims struct {
 	jwt.RegisteredClaims
 	UserId   int    `json:"user_id"`
 	Username string `json:"username"`
 }
 
+// функция для входа(получения jwt токена)
 func (s *UserService) SingIn(ctx *fiber.Ctx) error {
 	var input models.User
 
+	// парсим данные для входа
 	if err := ctx.BodyParser(&input); err != nil {
 		s.logger.Errorw("Error parsing body", "error", err)
 		return dto.BadResponseError(ctx, dto.FieldBadFormat, err.Error())
 	}
 
-	// получаем юзера из бд
+	// получаем юзера из бд по имени
 	user, err := s.repo.SingIn(ctx.Context(), input.Username)
 	if err != nil {
 		s.logger.Errorw("Error authorization", "error", err)
 		return dto.InternalServerError(ctx)
 	}
 
+	// сверяем пароль из БД с введенныи паролем
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
 		s.logger.Errorw("Error wrong password", "error", err)
 		return dto.BadResponseError(ctx, dto.FieldBadFormat, err.Error())
 	}
 
+	// создаем структуру для нашего jwt токена и передаем в него нужную информацию
 	claims := CustomClaims{
 		RegisteredClaims: jwt.RegisteredClaims{
 			Subject:   strconv.Itoa(user.Id),
@@ -91,8 +100,10 @@ func (s *UserService) SingIn(ctx *fiber.Ctx) error {
 		Username: input.Username,
 	}
 
+	// создание токена
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
+	// подписываем токен нашим ключем
 	tokenString, err := token.SignedString([]byte(s.JWTSecret))
 	if err != nil {
 		s.logger.Errorw("Error signing token", "error", err)
@@ -107,6 +118,7 @@ func (s *UserService) SingIn(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(response)
 }
 
+// эта функция без middleware, просто функция для удаления пользователя по тз с ON DELETE CASCADE
 func (s *UserService) DeleteUser(ctx *fiber.Ctx) error {
 	id, err := strconv.Atoi(ctx.Params("id"))
 	if err != nil {
